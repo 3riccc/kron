c184186bf9fe0c3be49e568338484c5b74fdfb54
mrz <ericinisa@163.com>
2021-02-24 10:15:12 +0800

    useless if we train by structure first and then optimize by dynamics

--- kron_train_dyn.py
+++ kron_train_dyn.py
@@ -14,11 +14,14 @@ from sklearn import metrics
 HYP={
     'node_num':128,
     'miss_percent':0.1,
-    'seed':2050,
+    'seed':2051,
     'batch_size':1024,
     'lr_net':0.001,
+    'lr_net_stru':0.001,
+    'lr_net_dyn':0.0001,
+    # 'lr_net_dyn':0.000003,
     'lr_dyn':0.0001,
-    'epoch':400
+    'epoch':30000
 }
 print(HYP)
 del_num = int(HYP['node_num']*HYP['miss_percent'])
@@ -75,27 +78,35 @@ def kronecker(A,B):
 class Gumbel_union_kron(nn.Module):
     def __init__(self,sz,k):
         super(Gumbel_union_kron,self).__init__()
-        self.p = Parameter(torch.Tensor(sz,sz).uniform_(0,1))
-        # self.p = Parameter(torch.tensor([[0.9902,0.3957],[0.4112,0.0761]]))
+        self.p = Parameter(torch.Tensor(sz,sz).uniform_(0,0.5))
+#         self.p = Parameter(torch.tensor([[0.92,0.4],[0.5,0.6]]))
         self.k = k
     
     # generate adj from original kernel
     def generate_adj(self):
-        t = torch.ones(self.p.shape).to(device)*0.99
-        p0 = torch.relu(self.p) - torch.relu(self.p-t)
-        adj = torch.relu(self.p) - torch.relu(self.p-t)
+        p0 = torch.relu(self.p-0.001)+0.001 - torch.relu(self.p-0.999)
+        adj = torch.relu(self.p-0.001)+0.001 - torch.relu(self.p-0.999)
         for i in range(self.k-1):
             adj = kronecker(adj,p0)
         return adj
+    
     def sample_all(self):
         p1 = self.generate_adj() # prob to get 1
         p0 = 1-p1 # prob to get 0
-        logits = torch.cat([p0.unsqueeze(2),p1.unsqueeze(2)],dim=2)
+        logits = torch.cat([p0.unsqueeze(2),p1.unsqueeze(2)],dim=2)#[node,node,2]
         # log it to suit for gumbel softmax
-        logits = torch.log(logits)
+        logits = torch.log(logits)#[node,node,2]
         # gumbel softmax
         sampled_adj = torch.nn.functional.gumbel_softmax(logits,hard=False,tau=1)[:,:,1]
         return sampled_adj
+    # generate p0 to init former gumbel generator
+    def generate_p0_for_init(self):
+        p1 = self.generate_adj() # prob to get 1
+        p0 = 1-p1 # prob to get 0
+        logits = torch.cat([p0.unsqueeze(2),p1.unsqueeze(2)],dim=2)#[node,node,2]
+        # log it to suit for gumbel softmax
+        logits = torch.log(logits)#[node,node,2]
+        return logits
 
 
 # normal network generator
@@ -120,6 +131,9 @@ def load_cml_ggn(data_path,batch_size = 128,node=10,seed=2050):
     
     print('\nMatrix dimension: %s \nTrain data size: %s \nVal data size: %s \nTest data size: %s'
           % (object_matrix.shape, train_data.shape, val_data.shape, test_data.shape))    
+
+    print('use 2000 instead of 10000')
+    # train_data = train_data[:2000]
 
     train_loader = DataLoader(train_data[:], batch_size=batch_size, shuffle=False)#
     val_loader = DataLoader(val_data[:], batch_size=batch_size, shuffle=False) # 记得改回来
@@ -154,15 +168,16 @@ def auc_unknown(p0,object_matrix,del_num):
     return area
 
 # load data
-# data_path = './data/2000cmlkron128-2500.pickle'
-data_path = './data/2000cmlkron-ws128-10000.pickle'
+data_path = './data/2000cmlkron-ws128-10000-00.pickle'
+# data_path = './data/2000cmlkron-10-100-10000-00.pickle'
 # a = torch.load(data_path,map_location=torch.device('cpu'))
 train_loader, val_loader, test_loader, object_matrix = load_cml_ggn(data_path,batch_size=HYP['batch_size'],node=HYP['node_num'])
 observed_adj = object_matrix[:-del_num,:-del_num]
 
-# generator = Gumbel_union_kron(2,7).to(device)
-generator = Gumbel_Generator(128).to(device)
-op_net = optim.Adam(generator.parameters(), lr=HYP['lr_net'])
+generator = Gumbel_union_kron(2,7).to(device)
+# generator = Gumbel_Generator(128).to(device)
+op_net = optim.Adam(generator.parameters(), lr=HYP['lr_net_stru'])
+# op_net_dyn = optim.Adam(generator.parameters(), lr=HYP['lr_net_dyn'])
 
 dyn = IO_B(1,128).to(device)
 op_dyn = optim.Adam(dyn.parameters(), lr=HYP['lr_dyn'])
@@ -182,7 +197,8 @@ def train_dyn_gen():
         
         # pred state for each node which can be observed
         for j in range(HYP['node_num']-del_num):
-            op_net.zero_grad()
+            op_init.zero_grad()
+            # op_net_dyn.zero_grad()
             op_uxg.zero_grad()
             op_dyn.zero_grad()
             
@@ -191,7 +207,8 @@ def train_dyn_gen():
             x_un_paed = train_uxg.get_x(b_idx)[:thisbatchsz] #[batchsz,unobserved_node,1]
             x_hypo = torch.cat([x,x_un_paed],dim=1) #[batchsz,node,1]
             
-            hypo_adj = generator.sample_all()
+            # hypo_adj = generator.sample_all()
+            hypo_adj = g_init.sample_all()
             
             # replace the observed part
             t0 = torch.ones(hypo_adj.shape).to(device)
@@ -205,23 +222,74 @@ def train_dyn_gen():
             loss = loss_f(yhat,y[:,j,:])
             loss.backward()
             
-            op_net.step()
+            op_init.step()
+            # op_net_dyn.step()
             op_dyn.step()
             op_uxg.step()
             losses.append(loss.item())
     return np.mean(losses)
 
+# train by observed structure
+def train_stru():
+    op_net.zero_grad()
+    hypo_adj = generator.generate_adj()
+    o_part = hypo_adj[:-del_num,:-del_num]
+    # print(o_part)
+    # print(observed_adj)
+    loss = loss_f(o_part,observed_adj.to(device))
+    loss.backward()
+    op_net.step()
+    return loss.item()
+
+
+
 areas = []
 losses = []
+loss_strus = []
 times = []
+
+# train by structure
+for i in range(500):
+    train_stru()
+#     print(generator.p)
+    p0 = generator.generate_adj().detach().cpu().numpy()
+    area = auc_unknown(p0,object_matrix,del_num)
+    print('auc:',area)
+
+# normal network generator
+class Gumbel_Generator_Init(nn.Module):
+    def __init__(self,init_p0):
+        super(Gumbel_Generator_Init,self).__init__()
+#         self.p = Parameter(torch.randn(sz,sz,2))
+        self.p = Parameter(init_p0)
+    def generate_adj(self):
+        p0 = torch.nn.functional.softmax(self.p,dim=2)[:,:,1]
+        symp0 = torch.triu(p0,diagonal=0)+torch.triu(p0,diagonal=1).transpose(0,1) # make it symmetric
+        return symp0
+    def sample_all(self):
+        sampled_adj = torch.nn.functional.gumbel_softmax(self.p,hard=False,tau=1)[:,:,1]
+        sym_adj = torch.triu(sampled_adj,diagonal=0)+torch.triu(sampled_adj,diagonal=1).transpose(0,1)# make it symmetric
+        return sym_adj
+
+init_p0 = generator.generate_p0_for_init().detach()
+g_init = Gumbel_Generator_Init(init_p0)
+op_init = optim.Adam(g_init.parameters(), lr=HYP['lr_net'])
+
 for i in range(HYP['epoch']):
-    # print(generator.p)
-    print('epoch:',i)
+    # train by dynamics info
+    print('epoch:',i+500)
+
     loss = train_dyn_gen()
     losses.append(loss)
     print('train loss:',loss)
+
+    # train by observed stru info
+    # lstru = train_stru()
+    # loss_strus.append(lstru)
+    # print('train structural loss:',lstru)
+
     # check auc
-    p0 = generator.generate_adj().cpu().detach().numpy()
+    p0 = g_init.generate_adj().cpu().detach().numpy()
     area = auc_unknown(p0,object_matrix,del_num)
     print('auc:',area)
     areas.append(area)
